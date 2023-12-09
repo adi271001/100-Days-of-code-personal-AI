62f5bd1375d693e7758ffd00c50ff64aafc73868
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-12-09 19:50:11 +0530

    Create Day9.py

---
+++ Day9.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        def helper(root,result):
+          if root != None:
+            helper(root.left,result)
+            result.append(root.val)
+            helper(root.right,result)       
+        result = []
+        helper(root,result)
+        return result
