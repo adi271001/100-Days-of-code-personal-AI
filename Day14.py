32238610a875586a86590b0b41d85784605dbd89
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-12-14 19:32:33 +0530

    Create Day14.py

---
+++ Day14.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def onesMinusZeros(self, grid):
+        m, n = len(grid), len(grid[0])
+        row_ones = [0] * m
+        col_ones = [0] * n
+        for i in range(m):
+            for j in range(n):
+                row_ones[i] += grid[i][j]
+                col_ones[j] += grid[i][j]
+        for i in range(m):
+            for j in range(n):
+                grid[i][j] = 2 * (row_ones[i] + col_ones[j]) - m - n
+        return grid
