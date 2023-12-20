ec3aeaf426b05a21205bc6edfea529941e7ead97
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-12-20 14:35:51 +0530

    Create Day20.py

---
+++ Day20.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def buyChoco(self, arr, key):
+        ans = float('inf')
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j:
+                    total = arr[i] + arr[j]
+                    ans = min(ans, total)
+        fin = key - ans
+        if(fin>=0):
+            return fin
+        else:
+            return key
